4d8511de964eab061ae02a6a10338fc7c5a72006
Gollasomesh <162982168+Gollasomesh@users.noreply.github.com>
2024-03-18 11:52:40 +0530

    Add files via upload

---
+++ file_7_.py
@@ -0,0 +1,117 @@
+# 1.)
+# d1 = {'ten':10, 'twenty': 20:, 'thirty':30}
+# d2 = {'thirty': 30, 'fourty': 40, 'fifty': 50}
+#       d1,updated(d2)
+#       print(d1)
+
+# 2.)python program to determine if
+# the given sets are disjoint
+# or not without using Inbuilt-in Functions
+
+# set1 = {'python', 'java', 'data science'}
+# set2 = {'ML', "AI', 'R Language', 'Python'}
+# set3 = {'data analytics', 'Robotics', 'Deep Learning'}
+# c = 0
+
+# if flag==0:
+#      print("Disjoint")
+# else:
+#     print("joint"
+
+
+# c = 0
+# flag = 0
+# for val in range(3):
+#     c=c+1
+#     if c==1:
+#         for val in set1:
+#             if val in set2 or val in set3:
+#                flag=1
+#                break
+#    if c==2:
+#        for val in  set2:
+#            if val in set1 or val in set3:
+#                flag=1
+#                break
+
+#    if c==3:
+#        for val in  set3:
+#            if val in set2 or val in set1:
+#                flag=1
+#                break
+# if flag==0:
+#     print("Disjoint")
+# else:
+#     print("joint"
+
+# 3.)list1 = ["M", "na", "i", "Ke"]
+# list2 = ["y", "me", "s", "lly"]
+
+# O/P --> ['My', 'name', 'is', 'Kelly']
+
+# l1 = ["M", "na", "i", "Ke"]
+# l2 = ["y", "me", "s", "lly"]
+
+# l3 = []
+# for val in range(len(list1)):
+#     ans = list1[val]+list2[val]
+#     l3.append(ans)
+# print(l3)
+
+# i=0
+# while i<len(list1):
+#     l3.append(list1[i]+list2[i])
+#     i+=1
+# print(l3)
+
+# ! Functions
+# ? DEF
+# Function is a block of code which is used to perform a specific functionality
+# Function can be created using def keyword
+
+# ? Function has 3 parts
+# Function declaration
+# Function defanition
+# Function call
+
+# def great():
+#     print("Greatings User!!")
+# great()
+# great() # function calling
+
+# ! Eg:2
+# def adding():
+#     d =a+b+c
+#
+#     print(d)
+# adding()
+# adding()
+
+def greeting():
+    print("Welcome user!!!")
+
+    
+greeting("siddu")
+greating("madhan")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
